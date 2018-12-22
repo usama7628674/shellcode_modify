3e8a52df55358f100f33ab1690aa12366e60213c
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-12-22 09:58:35 +0500

    Update shellcodemodify.py

--- shellcodemodify.py
+++ shellcodemodify.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 #This script will change \x41\x42\x43 to 0x41,0x42,0x43
+import os
 
-shllcode = input("Your shellcode file location (.txt) (/root/...): \n> ")
+shllcode = input("paste your shellcode \n> ")
 
-# Edit this line with the path to the binary file containing shellcode you are converting
-with open(shllcode, 'r+') as sc_handle:
-	sc_data = sc_handle.read()
 
-new_shellcode = "0" + ",0".join(sc_data.split("\\")[1:])
+new_shellcode = "0" + ",0".join(shllcode.split("\\")[1:])
+os.system('clear')
+print('\n')
 print(new_shellcode)
-
-
-
-
