31f1cb64caf94c60c4453d39a368352ed6206d5f
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-12-22 10:03:24 +0500

    Update shellcodemodify.py

--- shellcodemodify.py
+++ shellcodemodify.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 #This script will change \x41\x42\x43 to 0x41,0x42,0x43
 import os
+GREEN = '\033[1;32m'
+END = '\033[0m'
 
 shllcode = input("paste your shellcode \n> ")
 
 
 new_shellcode = "0" + ",0".join(shllcode.split("\\")[1:])
 os.system('clear')
-print('\n')
+print(GREEN + 'Your shellcode is converted' + END)
 print(new_shellcode)
